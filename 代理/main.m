b5bc4eced20628d179c557dba0ad5aa74625d4e1
rimi <rimi@rimideiMac-24.local>
2015-07-01 13:43:55 +0800

    Initial Commit

---
+++ 代理/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  代理
+//
+//  Created by rimi on 15/7/1.
+//  Copyright (c) 2015年 rimi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
